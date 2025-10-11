3bd0aeab5e056ae3c249e4a9f9e66951d19762ce
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2025-10-11 17:40:07 +0500

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,13 @@
+n=10
+n2=4
+
+print("n&n2=",n&n2)
+print("\nn|n2=",n|n2)
+print("\n~n=",~n)
+print("\nn^n2=",n^n2)
+
+print(f"\nBefore left shifting {n}. After left shifting value is: n<<1=",n<<1)
+print(f"\nBefore left shifting {n2}. After left shifting value is: n2<<1=",n2<<1)
+
+print(f"\nBefore right shifting {n}. After right shifting value is: n>>1=",n>>1)
+print(f"\nBefore right shifting {n2}. After right shifting value is: n2>>1=",n2>>1)
