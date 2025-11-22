2125645ec5b6dc07f45ec3540cfb924719916eb9
Barkamol0422 <ganijonovbarkamol04222011@gmail.com>
2025-11-22 16:29:00 +0500

    Add files via upload

--- act_2.py
+++ act_2.py
@@ -1,16 +1,19 @@
-def power8(number):
-    if number<=0:
-        return False
-    if number&(number-1):
-        return False
-    count=0
-    while number>1:
-        number>>=1
-        count+=1
-    return (count%3)==0
+def reverse_bits(n):
+    if n==0:
+        return 0,"0"
+    bl=n.bit_length()
+    rev=0
+    for i in range(bl):
+        rev=(rev<<1) | ((n>>i)&1)
+    return rev, format(rev,f'0{bl}b')
 
-number=int(input("Enter a number: "))
-if power8(number):
-    print(number,"is power of 8")
-else:
-    print(number,"is not power of 8")
+
+number=int(input("Enter your original number: "))
+
+if number<0:
+    print("Invalid input")
+    exit()
+rev, rev_bin=reverse_bits(number)
+orig_bin=format(number,f'0{number.bit_length()}b') if number>0 else "0"
+print(f"Enter your original number: {number} ({orig_bin})")
+print(f"Reversed Number : {rev} ({rev_bin})")
